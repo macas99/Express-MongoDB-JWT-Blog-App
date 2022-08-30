ec1bd484cedb6a8a223f272544d348133d68d3c5
macas99 <marvndude@gmail.com>
2022-08-30 16:31:38 +0200

    Changes to Post model

--- models/Post.js
+++ models/Post.js
@@ -2,11 +2,33 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const getDate = function () {
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    if (day < 10) day = "0" + day;
+    if (month < 10) month = "0" + month;
+    let date = `${day}/${month}/${year}`;
+    return date;
+}
+
+function getTime() {
+    let today = new Date();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    if (hour < 10) hour = "0" + hour;
+    if (minute < 10) minute = "0" + minute;
+    let time = `${hour}:${minute}`;
+    return time;
+}
+
 const postSchema = new Schema({
     title: { type: String, trim: true, required: true },
     author: { type: String, trim: true, required: true },
     body: { type: String, trim: true, required: true },
-    createdAt: { type: Date, default: Date.now },
+    date: { type: String, default: getDate() },
+    time: { type: String, default: getTime() },
     likes: { type: Number, default: 0 }
 })
 
