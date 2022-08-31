55e436149b33fb1f71af82858746a8504293fab2
macas99 <marvndude@gmail.com>
2022-08-31 22:52:44 +0200

    Posts now sorted by date new --> old

--- services/post.service.js
+++ services/post.service.js
@@ -1,8 +1,22 @@
 const Post = require('../models/Post');
 
 const getPostsByFollowing = async function (userArray) {
-    const posts = await Post.find( { author: { $in: userArray } } );
+    const posts = await Post.find({ author: { $in: userArray } });
+    return sortByDate(posts);
+}
+
+const sortByDate = function (posts) {
+    posts.sort(function (a, b) {
+        return stringToDate(b.date, b.time) - stringToDate(a.date, a.time);
+    });
     return posts;
+}
+
+//cant parse DD/MM/YYYY format to Date()
+const stringToDate = function (date, time) {
+    const dateSplit = date.split("/");
+    const dateObject = new Date(dateSplit[2] + "-" + dateSplit[1] + "-" + dateSplit[0] + "T" + time);
+    return dateObject;
 }
 
 module.exports = {
