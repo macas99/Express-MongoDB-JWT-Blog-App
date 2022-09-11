b0cb9154b8bde55f130c95a49bc07cefdf2bdbd2
macas99 <marvndude@gmail.com>
2022-09-11 18:41:52 +0200

    Search route, ejs partials (navbar)

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,11 +2,13 @@ const express = require('express');
 const home = require('./home.route');
 const user = require('./user.route');
 const post = require('./post.route');
+const search = require('./search.route');
 
 const router = express.Router();
 
 router.use('/', home);
 router.use('/user', user);
 router.use('/posts', post);
+router.use('/search', search);
 
 module.exports = router;
